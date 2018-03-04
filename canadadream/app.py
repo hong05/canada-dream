23463cd5844bea5034c16ad6c563b642d2daae9e
hong05 <naichaer05@gmail.com>
2018-03-04 13:35:54 -0700

    fix css and images

--- canadadream/app.py
+++ canadadream/app.py
@@ -20,11 +20,7 @@ Powders = Powders()
 
 @app.route('/')
 def index():
-    return render_template('home.html')
-
-@app.route('/about')
-def about():
-    return render_template('about.html')
+    return render_template('index.html')
 
 @app.route('/powders')
 def powders():
