203f4c099d8ff6bb47ba738946cf14e04cb95d64
hong wang <hongwang@hongdeiMac.hitronhub.home>
2018-03-03 17:02:23 -0700

    setup mySQL for registration

--- canadadream/app.py
+++ canadadream/app.py
@@ -1,7 +1,20 @@
-from flask import Flask,render_template
+from flask import Flask,render_template,flash,redirect,url_for,session,logging,request
 from data import Powders
+from flask_mysqldb import MySQL
+from wtforms import Form,StringField,TextAreaField,PasswordField,validators
+from passlib.hash import sha256_crypt
 
 app = Flask(__name__)
+
+#config MySQL
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = 'wxwh0505'
+app.config['MYSQL_DB'] = 'canadadream'
+app.config['MYSQL_CURSORCLASS'] = 'DictCursor'
+#init MYSQL
+mysql = MySQL(app)
+
 
 Powders = Powders()
 
@@ -21,5 +34,35 @@ def powders():
 def powder(id):
     return render_template('powder.html',id=id)
 
+class RegisterForm(Form):
+    name = StringField('Name',[validators.Length(min=1,max=50)])
+    username = StringField('Username',[validators.Length(min=1,max=20)])
+    password = PasswordField('Password',[validators.Length(min=1,max=20)])
+    email = StringField('Email',[validators.DataRequired()])
+
+@app.route('/register',methods = ['GET','POST'],)
+def register():
+    form = RegisterForm(request.form)
+    if request.method =='POST' and form.validate():
+        name = form.name.data
+        username =form.username.data
+        password =sha256_crypt.encrypt(str(form.password.data))
+        email=form.email.data
+
+        #create cursor
+        cur = mysql.connection.cursor()
+        cur.execute("INSERT INTO users(name,username,password,email) Values(%s,%s,%s,%s)",(name,username,password,email))
+
+        #commit to db
+        mysql.connection.commit()
+
+        cur.close()
+        flash("Successfully Registered",'success')
+        return redirect(url_for('index'))
+
+    return render_template('register.html',form=form)
+
+
 if __name__=='__main__':
+    app.secret_key = 'canadadream'
     app.run(debug=True)
